37754df0a4fadc220b21852a2d034b95ecef84ba
deboracaires <debora.caires.mor@gmail.com>
2021-07-25 13:07:23 -0300

    botao de fazer pedido habilita

--- scripts.js
+++ scripts.js
@@ -1,28 +1,64 @@
+let pratoSelecionado = 0;
+let bebidaSelecionado = 0;
+let sobremesaSelecionado = 0;
 
 
 function selecionarPrato(elemento){
 
     const botaoMarcado = document.querySelector(".prato.selecionado");
     if(botaoMarcado !== null){
-        botaoMarcado.classList.remove("selecionado")
+        botaoMarcado.classList.remove("selecionado");
+        pratoSelecionado = 1;
     }
     elemento.classList.add("selecionado");
+    pratoSelecionado = 1;
+    habilitarPedido();
+    
 }
+
+
 
 function selecionarBebida(elemento){
 
     const botaoMarcado = document.querySelector(".bebida.selecionado");
     if(botaoMarcado !== null){
-        botaoMarcado.classList.remove("selecionado")
+        botaoMarcado.classList.remove("selecionado");
+        bebidaSelecionado = 1;
+
     }
     elemento.classList.add("selecionado");
+    bebidaSelecionado = 1;
+    habilitarPedido();
+    
+    
 }
-
+ 
 function selecionarSobremesa(elemento){
 
     const botaoMarcado = document.querySelector(".sobremesa.selecionado");
     if(botaoMarcado !== null){
-        botaoMarcado.classList.remove("selecionado")
+        botaoMarcado.classList.remove("selecionado");
+        sobremesaSelecionado = 1;
     }
     elemento.classList.add("selecionado");
+    sobremesaSelecionado = 1;
+    habilitarPedido();
+    
+}
+function habilitarPedido(){
+    const elemento = document.querySelector(".selecionar-pedido");
+    if(pratoSelecionado === 1 && bebidaSelecionado === 1 && sobremesaSelecionado === 1){
+        elemento.classList.add("pedido-finalizado");
+    }
+}
+
+
+function finalizarPedido(elemento){
+    const botaoMarcado = document.querySelector(".selecionar-pedido");
+    if(pratoSelecionado === 1 && bebidaSelecionado === 1 && sobremesaSelecionado === 1){
+        elemento.getElementById("selecionar-pedido").disabled = true;
+    }else{
+        elemento.getElementById("selecionar-pedido").disabled = false;
+    }
+    
 }
